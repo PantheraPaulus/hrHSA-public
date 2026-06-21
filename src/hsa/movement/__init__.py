ea1f403526f557c7538784765bc31d55a761a65a
Paul Kasko <paul.kasko@tum.de>
2026-06-21 18:09:19 +0200

    Add movement module exports

---
+++ src/hsa/movement/__init__.py
@@ -0,0 +1,12 @@
+"""Movement-geometry and movement-kernel tools."""
+
+from hsa.movement.geometry import prepare_trajectory_data, build_step_data, build_turn_angle_data
+from hsa.movement.distributions import fit_step_distribution, fit_turn_angle_distribution
+
+__all__ = [
+    "prepare_trajectory_data",
+    "build_step_data",
+    "build_turn_angle_data",
+    "fit_step_distribution",
+    "fit_turn_angle_distribution",
+]
