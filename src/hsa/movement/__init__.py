a72745f81a1114bfa0113698ff33d68209d85ec4
Paul Kasko <paul.kasko@tum.de>
2026-06-21 18:28:47 +0200

    Export movement kernel fitter

--- src/hsa/movement/__init__.py
+++ src/hsa/movement/__init__.py
@@ -2,6 +2,7 @@
 
 from hsa.movement.geometry import prepare_trajectory_data, build_step_data, build_turn_angle_data
 from hsa.movement.distributions import fit_step_distribution, fit_turn_angle_distribution
+from hsa.movement.kernel import fit_movement_kernel_per_id
 
 __all__ = [
     "prepare_trajectory_data",
@@ -9,4 +10,5 @@ __all__ = [
     "build_turn_angle_data",
     "fit_step_distribution",
     "fit_turn_angle_distribution",
+    "fit_movement_kernel_per_id",
 ]
