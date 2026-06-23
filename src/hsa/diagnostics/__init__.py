e846d7d7f7301617e457dcc624d63d80fe43f907
Paul Kasko <paul.kasko@tum.de>
2026-06-23 13:50:55 +0200

    Add diagnostics package

---
+++ src/hsa/diagnostics/__init__.py
@@ -0,0 +1,19 @@
+"""Diagnostic tools for inspecting used/available predictor structure."""
+
+from hsa.diagnostics.predictors import (
+    common_language_effect_size,
+    inspect_predictors,
+    plot_categorical_bars,
+    plot_continuous_ecdfs,
+    summarize_categorical,
+    summarize_continuous,
+)
+
+__all__ = [
+    "common_language_effect_size",
+    "inspect_predictors",
+    "plot_categorical_bars",
+    "plot_continuous_ecdfs",
+    "summarize_categorical",
+    "summarize_continuous",
+]
